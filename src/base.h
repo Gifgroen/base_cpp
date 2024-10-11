66783fa147052ef77ea2a9294aa711fa2fd81f18
Karsten Westra <karsten@gpal.nl>
2024-10-11 16:24:05 +0200

    Added context detection for OS and ARCH.

--- src/base.h
+++ src/base.h
@@ -1,27 +1,90 @@
 #ifndef _BASE_H
 #define _BASE_H
 
-#if defined(_MSC_VER)
-#  define COMPILER_CL 1
-#elif defined(__clang__)
+///
+/// Detect program context
+///
+
+#if defined(__clang__) /// Compiler: clang
 #  define COMPILER_CLANG 1
-#elif defined(__GNUC__)
+#  if defined(_WIN_32) // OS
+#    define OS_WINDOWS 1
+#  elif defined(__APPLE__) && defined(__MACH__)
+#    define OS_MAC 1
+#  elif defined(__linux__)
+#    define OS_LINUX 1
+#  else
+#    error Unsupported OS for this compiler setup
+#  endif
+#  if defined(__arm__) // Arch
+#    define ARCH_ARM 1
+#  elif defined(__aarch64__)
+#    define ARCH_ARM64 1
+#  elif defined(i386) || defined(__i386) || defined(__i386__)
+#    define ARCH_x86 1
+#  elif defined(__x86_64__)
+#    define ARCH_x64 1
+#  else
+#    error Undefined architecture
+#  endif
+#elif defined(_MSC_VER) /// Compiler: MSVC
+#  define COMPILER_CL 1
+#  if defined(_WIN_32) // OS
+#    define OS_WINDOWS 1
+#  else
+#    error Unsupported OS for this compiler setup
+#  endif
+
+  /// TODO: ARCH
+
+#elif defined(__GNUC__) // COMPILER GCC
 #  define COMPILER_GCC 1
+  /// TODO: OS
+#   error Unsupported OS for this compiler setup
+  /// TODO: ARCH
+#   error Unsupported ARCH for this compiler setup
 #else
-#  error Unknown compiler
+#  error Unsupported compiler
 #endif
 
 ///
+/// Zero out undefined context
+///
+
 /// Compiler
-/// 
-#if !defined(COMPILER_CL)
-#  define COMPILER_CL 0
-#endif
 #if !defined(COMPILER_CLANG)
 #  define COMPILER_CLANG 0
 #endif
+#if !defined(COMPILER_CL)
+#  define COMPILER_CL 0
+#endif
 #if !defined(COMPILER_GCC)
 #  define COMPILER_GCC 0
+#endif
+
+/// OS
+#if !defined(OS_WINDOWS)
+#  define OS_WINDOWS 0
+#endif
+#if !defined(OS_MAC)
+#  define OS_MAC 0
+#endif
+#if !defined(OS_LINUX)
+#  define OS_LINUX 0
+#endif
+
+/// ARCH
+#if !defined(ARCH_ARM)
+#define ARCH_ARM 0
+#endif
+#if !defined(ARCH_ARM64)
+#define ARCH_ARM64 0
+#endif
+#if !defined(ARCH_x86)
+#define ARCH_x86 0
+#endif
+#if !defined(ARCH_x64)
+#define ARCH_x64 0
 #endif
 
 #endif // _BASE_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,21 @@ int main(int Argc, char **Argv)
     printf("Hello world!\n");
 
     printf("\n=== context ===\n");
-
+    printf("\n= compiler =\n");
     printf("CL = %d\n", COMPILER_CL);
     printf("CLANG = %d\n", COMPILER_CLANG);
     printf("GCC = %d\n", COMPILER_GCC);
+
+    printf("\n= OS =\n");
+    printf("OS_WINDOWS %d\n", OS_WINDOWS);
+    printf("OS_MAC %d\n", OS_MAC);
+    printf("OS_LINUX %d\n", OS_LINUX);
+
+    printf("\n= ARCH =\n");
+    printf("ARCH_ARM = %d\n", ARCH_ARM);
+    printf("ARCH_ARM64 = %d\n", ARCH_ARM64);
+    printf("ARCH_x86 = %d\n", ARCH_x86);
+    printf("ARCH_x64 = %d\n", ARCH_x64);
 
     return 0;
 }
